3953917300befa0bc4b57ed900c1b7b2b1224cc2
Adon Metcalfe <adonm@fastmail.fm>
2017-08-04 21:31:25 +0800

    doh merge better

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -6,9 +6,5 @@ import java.io.PrintWriter
 import sys.process._
 
 object Main extends App {
-<<<<<<< HEAD
-  println("Hello, world!")
-=======
   println("16:{\"me\":\"blinken\"}")
->>>>>>> 58eba7e97e9f4116b72445ec0b16c56d710d1e6c
 }

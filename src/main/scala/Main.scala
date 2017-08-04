885660280163c66e1bb4c5405efba9ddfe94a669
Patrick Coleman <blinken@gmail.com>
2017-08-04 13:44:12 +0000

    SOCKET

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -7,4 +7,25 @@ import sys.process._
 
 object Main extends App {
   println("16:{\"me\":\"blinken\"}")
+
+  // Simple client
+  import java.net._
+  import java.io._
+  import scala.io._
+
+  val s = new Socket(InetAddress.getByName("punter.inf.ed.ac.uk"), 9001)
+  //val s = new Socket(InetAddress.getByName("canireachthe.cloud"), 80)
+  val is = new BufferedSource(s.getInputStream())
+  val out = new PrintStream(s.getOutputStream())
+
+  out.println("16:{\"me\":\"blinken\"\n\n")
+  out.flush()
+  Thread sleep 1000
+
+  println(is.mkString)
+
+  //while (in.hasNext) 
+  //  print(in.next())
+
+  s.close()
 }

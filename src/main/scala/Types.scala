93e57f2ea4ac282b2b534c75ee2f26ca7fbbec43
Scott Percival <code@moral.net.au>
2017-08-05 20:19:36 +0800

    😱😱😱😱😱😱😱😱😱😱

--- src/main/scala/Types.scala
+++ src/main/scala/Types.scala
@@ -6,6 +6,9 @@ import scalax.collection.GraphPredef._
 import scala.math.pow
 import scala.util.Random
 
+import io.circe._,  io.circe.generic.auto._, io.circe.parser._, io.circe.syntax._
+
+
 object Types {
 	type PunterId = Int
 	type SiteId = Int
@@ -29,4 +32,20 @@ object Types {
 	case class Pass(punter: PunterId) extends Move(punter)
 
 	type Score = (PunterId, Int)
+
+    implicit val encodeGraph: Encoder[Graph[SiteId, UnDiEdge]] = new Encoder[Graph[SiteId, UnDiEdge]] {
+         final def apply(a: Graph[SiteId, UnDiEdge]): Json = (Json.obj( ("nodes", a.nodes.map((x: Graph[SiteId,UnDiEdge]#NodeT) => x.value).asJson), ("edges", (for {
+            edges <- a.edges
+         } yield {
+            (edges._1.value, edges._2.value)
+         }).asJson) ))
+     }
+
+     implicit val decodeGraph: Decoder[Graph[SiteId, UnDiEdge]] = new Decoder[Graph[SiteId, UnDiEdge]] {
+         final def apply(c: HCursor): Decoder.Result[Graph[SiteId, UnDiEdge]] = Right(Graph.from(
+            (c.downField("nodes").focus.head.asArray.get.map((x => x.as[SiteId].right.get))),
+            (c.downField("edges").focus.head.asArray.get.map((x => x.asArray.get(0).as[SiteId].right.get ~ x.asArray.get(1).as[SiteId].right.get)))
+         ))
+     }
+
 }

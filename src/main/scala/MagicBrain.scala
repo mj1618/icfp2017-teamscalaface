5bf01e76b8ce2ee5b7b54d80318c954f44fa9bc0
Adon Metcalfe <adonm@fastmail.fm>
2017-08-05 12:29:47 +0800

    trying to implement score weighted algo

--- src/main/scala/MagicBrain.scala
+++ src/main/scala/MagicBrain.scala
@@ -4,6 +4,7 @@ package traceur
 import scalax.collection.Graph
 import scalax.collection.GraphEdge.UnDiEdge
 import scalax.collection.GraphPredef._
+import scala.math.pow
 
 import lambda.traceur.Types._
 import lambda.traceur.onlinemsg.Msg._ // for R_map, which probably belongs in Types
@@ -12,10 +13,13 @@ import lambda.traceur.helpers.Helpers._
 class ClaimedEdges(
   val us: Int,
   val numPlayers: Int,
+  val mines: List[SiteId],
   var graph: Graph[SiteId, UnDiEdge] // graph of unclaimed + our claimed edges
 ) extends State[ClaimedEdges] {
 
   var our_graph: Graph[SiteId, UnDiEdge] = Graph() // our claimed edges
+  //var targetEdges: List[UnDiEdge[SiteId]]
+  //var targetNodes: List[SiteId]
 
   override def update(claimed: List[(PunterId, River)]) : ClaimedEdges = {
     // remove an edge *and nodes*, if they are disconnected
@@ -24,13 +28,13 @@ class ClaimedEdges(
     for ((punter, river) <- claimed) {
       val edge = river.source~river.target
       graph = graph -! edge
-      punter match {
-        case `us` => our_graph = our_graph + edge
+      our_graph = punter match {
+        case `us` => our_graph + edge
         // also remove foreign-claimed edges from our graph, in case we tried to claim the same edge as someone else and they won
-        case _ => our_graph = our_graph -! edge
+        case _ => our_graph -! edge
       }
     }
-    debug("next: game graph  o+u: " + graph)
+    debug(s"next: game graph  o+u: $graph")
 
     return this
   }
@@ -38,7 +42,25 @@ class ClaimedEdges(
 
 class MagicBrain extends Brains[ClaimedEdges] {
   override def init(me: PunterId, numPlayers: Int, map: R_map) : ClaimedEdges = {
-    new ClaimedEdges(me, numPlayers, mapToGraph(map))
+    selectTargets(new ClaimedEdges(me, numPlayers, map.mines, mapToGraph(map)))
+  }
+
+  // calculate what to claim on map
+  def selectTargets(state: ClaimedEdges) : ClaimedEdges = {
+    debug(s"There are ${state.mines.size} mines, ${state.graph.edges.size} rivers and ${state.graph.nodes.size} sites in this map.")
+    var targetScore = 0.0
+    for (startmine <- state.mines) {
+      val node = state.graph.get(startmine)
+      var score = 0.0
+      for (mine <- state.mines) {
+        /* val path = node shortestPathTo state.graph.get(mine)
+        if (!path.isEmpty) {
+          score = score + pow(path.get.edges.size, 2)
+        }*/
+      }
+      println(s"mine $node score $score")
+    }
+    state
   }
 
   override def nextMove(state: ClaimedEdges) : River = {
